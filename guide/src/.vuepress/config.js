ca4cff0e129230ed882e7dbbd57d024e46fa854d
JinKanai <jin@kanaisan.com>
2025-08-09 00:08:01 +0000

    initial commit

---
+++ guide/src/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    /**
+     * Ref：https://v1.vuepress.vuejs.org/config/#title
+     */
+    title: 'クラウド総合演習 演習ガイド',
+    /**
+     * Ref：https://v1.vuepress.vuejs.org/config/#description
+     */
+    description: '演習手順 ご案内ページ' ,
+    dest: '../docs/',
+    base: '/cloud-capstone-project/',
+  }
